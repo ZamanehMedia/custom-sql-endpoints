d24867a2caa5602e6e0294c12e8b0ca7f9350941
Marcel Oomens <mail@marceloomens.com>
2024-03-18 22:37:20 +0100

    Initial feature stub

--- src/custom-sql-endpoints.php
+++ src/custom-sql-endpoints.php
@@ -21,9 +21,37 @@
  * License URI:       https://spdx.org/licenses/MIT.html
  * Update URI:        https://example.com/my-plugin/
  */
- 
-const NAMESPACE = 'custom_sql';
+
+define( 'CUSTOM_NAMESPACE', 'custom-sql/v1' );
 
 \add_action( 'rest_api_init', function() {
 	
+    \register_rest_route( CUSTOM_NAMESPACE, '/signature-distribution', array(
+        'methods' => 'GET',
+        'callback' => function ( \WP_REST_Request $request ) {
+            
+            global $wpdb;
+            $wpdb->show_errors();
+            
+            $params = $request->get_params();
+            
+            $query = "SELECT m.meta_value, count(*) FROM wp_comments AS c INNER JOIN wp_commentmeta AS m ON c.comment_ID = m.comment_id WHERE c.comment_post_ID IN ({petitions}) AND m.meta_key = 'dk_signature_location' GROUP BY m.meta_value";
+            
+            if (preg_match('/{(.+)}/', $query, $matches)) {
+               for ($i=1; $i<count($matches); $i++) {
+                    if (array_key_exists($matches[$i], $params)) {
+                       $query = preg_replace("/{{$matches[$i]}}/", \esc_sql($params[$matches[$i]]), $query);
+                    } else {
+                        // emit missing parameter error
+                    }
+               } 
+            }        
+            
+            $result = $wpdb->get_results( $wpdb->prepare( $query ), ARRAY_N );
+            
+            return array( $wpdb->last_query, $result );
+        },
+        'permission_callback' => '__return_true'
+    ) );
+    
 } );
