5e12256ffd4f0167d3958ff8f39de14a51ffe4fd
Marcel Oomens <mail@marceloomens.com>
2024-03-18 19:27:11 +0100

    Initial commit

---
+++ src/custom-sql-endpoints.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom SQL Endpoints
+ *
+ * @package           CustomSQLEndpoints
+ * @author            Marcel Oomens
+ * @copyright         2024 Zamaneh Media Foundation
+ * @license           ISC
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Custom SQL Endpoints
+ * Plugin URI:        https://451.tools/utilities/custom-sql-endpoints
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.2
+ * Author:            Marcel Oomens
+ * Author URI:        https://451.tools
+ * Text Domain:       plugin-slug
+ * License:           ICS
+ * - License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * - Update URI:        https://example.com/my-plugin/
+ */
